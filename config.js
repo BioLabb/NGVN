f49b88a538fa668d7b9d2ac76925e2e0d31fecb4
sinhtien <sinhtienit@gmail.com>
2021-11-19 23:27:06 +0700

    upAudio

--- config.js
+++ config.js
@@ -14,3 +14,7 @@ const COLOR_13 = "24, 220, 255";
 const ARR_COLOR = [COLOR_1,COLOR_2,COLOR_3,COLOR_4,COLOR_5,COLOR_6,COLOR_7,COLOR_8,COLOR_9,COLOR_10,COLOR_11,COLOR_12,COLOR_13];
 
 const OPACTY = 0.4;
+
+
+
+console.log(document.querySelector(".audio").play())
